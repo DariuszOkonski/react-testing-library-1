6a37eb4e238fe12d19c532260490c61bb0083691
Dariusz Okoński <darek200180@gmail.com>
2023-11-03 06:51:43 +0100

    find inputs by getByRole

--- src/UserForm.test.js
+++ src/UserForm.test.js
@@ -16,7 +16,9 @@ test('it calls onUserAdd when the form is submitted', () => {
   const mock = jest.fn();
   render(<UserForm onUserAdd={mock} />);
 
-  const [nameInput, emailInput] = screen.getAllByRole('textbox');
+  // const [nameInput, emailInput] = screen.getAllByRole('textbox');
+  const nameInput = screen.getByRole('textbox', { name: /name/i });
+  const emailInput = screen.getByRole('textbox', { name: /enter email/i });
 
   user.click(nameInput);
   user.keyboard('jane');
